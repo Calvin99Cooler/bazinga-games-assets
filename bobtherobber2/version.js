32c0f1c5ee7232016a229e32618ed1d08d45f2a6
Calvin99Cooler <jamal.marlow858@hotmail.com>
2025-10-17 16:07:18 -0500

    Update version.js

--- bobtherobber2/version.js
+++ bobtherobber2/version.js
@@ -1,2 +1,2 @@
 version="7";
-libs=["js/phaser/phaser-cachebuster.min.js","js/phaser/phaser-input.min.js","js/phaser/phaser-nineslice.min.js","js/phaser/phaser-spine.min.js","js/phaser/phaser-super-storage.min.js"];
+libs=["bobtherobber2/js/phaser/phaser-cachebuster.min.js","bobtherobber2/js/phaser/phaser-input.min.js","bobtherobber2/js/phaser/phaser-nineslice.min.js","bobtherobber2/js/phaser/phaser-spine.min.js","bobtherobber2/js/phaser/phaser-super-storage.min.js"];
