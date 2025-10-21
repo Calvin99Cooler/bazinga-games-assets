23f42e4753b8dba3a5c2d7339c17b29e0878582a
Calvin99Cooler <jamal.marlow858@hotmail.com>
2025-10-21 07:58:31 -0500

    Update master-loader.js

--- gladih/master-loader.js
+++ gladih/master-loader.js
@@ -3,10 +3,10 @@ var scripts = document.getElementsByTagName("script"),
     scriptUrl = scripts[scripts.length - 1].src,
     root = scriptUrl.split("master-loader.js")[0],
     loaders = {
-        unity: "unity.js"
+        unity: "https://cdn.jsdelivr.net/gh/Calvin99Cooler/bazinga-games-assets@main/gladih/unity.js"
     };
 if (0 <= window.location.href.indexOf("pokiForceLocalLoader") 
-	&& (loaders.unity = "./unity.js", 
+	&& (loaders.unity = "https://cdn.jsdelivr.net/gh/Calvin99Cooler/bazinga-games-assets@main/gladih/unity.js", 
 		
 		root = "/loaders"), !window.config) throw Error("window.config not found");
 var loader = loaders[window.config.loader];
@@ -15,7 +15,7 @@ if (!window.config.unityWebglLoaderUrl) {
     var versionSplit = window.config.unityVersion ? window.config.unityVersion.split(".") : [],
         year = versionSplit[0],
         minor = versionSplit[1];
-          window.config.unityWebglLoaderUrl ="./UnityLoader.2019.2.js";
+          window.config.unityWebglLoaderUrl ="https://cdn.jsdelivr.net/gh/Calvin99Cooler/bazinga-games-assets@main/gladih/UnityLoader.2019.2.js";
     // switch (year) {
     //     case "2019":
     //         window.config.unityWebglLoaderUrl = 1 === minor ? "./UnityLoader.2019.1.js" : "./UnityLoader.2019.2.js";
@@ -25,7 +25,7 @@ if (!window.config.unityWebglLoaderUrl) {
     // }
 }
 var sdkScript = document.createElement("script");
-sdkScript.src = "./poki-sdk.js", sdkScript.onload = function() {
+sdkScript.src = "https://cdn.jsdelivr.net/gh/Calvin99Cooler/bazinga-games-assets@8901b847539a63757203319877a2aa222b7a8b4d/gladih/poki-sdk.js", sdkScript.onload = function() {
     var i = document.createElement("script");
     i.src = root + loader, document.body.appendChild(i)
 }, document.body.appendChild(sdkScript);
