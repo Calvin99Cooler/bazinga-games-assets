984ddad1879bae55b195928ec047b5d34031cf71
Calvin99Cooler <jamal.marlow858@hotmail.com>
2025-10-20 20:21:32 -0500

    Update poki-sdk.js

--- subway-surfers/poki-sdk.js
+++ subway-surfers/poki-sdk.js
@@ -1,62 +1,69 @@
 (() => {
-    const o = new (function () {
-        function SDKLoader() {
+    // base URL for your assets
+    const BASE_URL = "https://cdn.jsdelivr.net/gh/Calvin99Cooler/bazinga-games-assets@main/subway-surfers/";
+
+    // PokiSDK placeholder
+    const PokiSDKQueue = [];
+    let PokiSDKInstance;
+
+    class PokiSDKWrapper {
+        constructor() {
             this.queue = [];
-            this.init = (options = {}) => new Promise((resolve, reject) => this.enqueue("init", options, resolve, reject));
-            this.rewardedBreak = () => new Promise(resolve => resolve(false));
-            this.noArguments = (method) => () => this.enqueue(method);
-            this.oneArgument = (method) => (arg) => this.enqueue(method, arg);
-            this.handleAutoResolvePromise = () => new Promise(resolve => resolve());
-            this.handleAutoResolvePromiseObj = () => new Promise(resolve => resolve());
-            this.throwNotLoaded = () => console.debug("PokiSDK is not loaded yet. Not all methods are available.");
         }
-
-        SDKLoader.prototype.enqueue = function(method, options, resolve, reject) {
-            this.queue.push({ fn: method, options, resolveFn: resolve, rejectFn: reject });
-        };
-
-        SDKLoader.prototype.dequeue = function() {
-            while (this.queue.length > 0) {
-                const item = this.queue.shift();
-                const fn = window.PokiSDK[item.fn];
-                if (typeof fn === "function") {
+        init(options = {}) {
+            return new Promise((resolve, reject) => this.enqueue("init", options, resolve, reject));
+        }
+        rewardedBreak() { return Promise.resolve(false); }
+        noArguments(fnName) { return () => this.enqueue(fnName); }
+        oneArgument(fnName) { return (arg) => this.enqueue(fnName, arg); }
+        handleAutoResolvePromise() { return Promise.resolve(); }
+        handleAutoResolvePromiseObj() { return Promise.resolve(); }
+        throwNotLoaded() { console.debug("PokiSDK not loaded yet."); }
+        enqueue(fn, options, resolve, reject) {
+            const q = { fn, options, resolveFn: resolve, rejectFn: reject };
+            PokiSDKQueue.push(q);
+        }
+        dequeue() {
+            while (PokiSDKQueue.length > 0) {
+                const item = PokiSDKQueue.shift();
+                const fn = item.fn;
+                if (typeof window.PokiSDK[fn] === "function") {
                     if (item.resolveFn || item.rejectFn) {
-                        fn(item.options)
-                            .then(...(item.resolveFn ? [item.resolveFn] : []))
-                            .catch(...(item.rejectFn ? [item.rejectFn] : []));
+                        window.PokiSDK[fn](item.options)
+                            .then(res => item.resolveFn && item.resolveFn(res))
+                            .catch(err => item.rejectFn && item.rejectFn(err));
                     } else {
-                        fn(item.options);
+                        window.PokiSDK[fn](item.options);
                     }
-                } else {
-                    console.error("Cannot execute " + item.fn);
                 }
             }
-        };
+        }
+    }
 
-        return SDKLoader;
-    })();
+    PokiSDKInstance = new PokiSDKWrapper();
 
+    // Attach placeholder methods first
     window.PokiSDK = {
-        init: o.init,
-        initWithVideoHB: o.init,
-        customEvent: o.throwNotLoaded,
-        destroyAd: o.throwNotLoaded,
-        getLeaderboard: o.handleAutoResolvePromiseObj
+        init: PokiSDKInstance.init.bind(PokiSDKInstance),
+        initWithVideoHB: PokiSDKInstance.init.bind(PokiSDKInstance),
+        customEvent: PokiSDKInstance.throwNotLoaded.bind(PokiSDKInstance),
+        destroyAd: PokiSDKInstance.throwNotLoaded.bind(PokiSDKInstance),
+        getLeaderboard: PokiSDKInstance.handleAutoResolvePromiseObj.bind(PokiSDKInstance),
     };
 
-    ["disableProgrammatic", "gameLoadingStart", "gameLoadingFinished", "gameInteractive", "roundStart", "roundEnd", "muteAd"].forEach(m => {
-        window.PokiSDK[m] = o.noArguments(m);
-    });
+    ["disableProgrammatic","gameLoadingStart","gameLoadingFinished","gameInteractive","roundStart","roundEnd","muteAd"]
+        .forEach(fn => { window.PokiSDK[fn] = PokiSDKInstance.noArguments(fn); });
 
-    ["setDebug", "gameplayStart", "gameplayStop", "gameLoadingProgress", "happyTime", "setPlayerAge", "togglePlayerAdvertisingConsent", "toggleNonPersonalized", "setConsentString", "logError", "sendHighscore", "setDebugTouchOverlayController"].forEach(m => {
-        window.PokiSDK[m] = o.oneArgument(m);
-    });
+    ["setDebug","gameplayStart","gameplayStop","gameLoadingProgress","happyTime","setPlayerAge",
+    "togglePlayerAdvertisingConsent","toggleNonPersonalized","setConsentString","logError",
+    "sendHighscore","setDebugTouchOverlayController"]
+        .forEach(fn => { window.PokiSDK[fn] = PokiSDKInstance.oneArgument(fn); });
 
-    // **Load the SDK from your fixed URL**
+    // Load actual SDK script
     const sdkScript = document.createElement("script");
-    sdkScript.src = "https://cdn.jsdelivr.net/gh/Calvin99Cooler/bazinga-games-assets@main/subway-surfers/poki-sdk-core-v2.234.2.js";
+    sdkScript.src = BASE_URL + "poki-sdk-core-v2.234.2.js";
     sdkScript.type = "text/javascript";
     sdkScript.crossOrigin = "anonymous";
-    sdkScript.onload = () => o.dequeue();
+    sdkScript.onload = () => PokiSDKInstance.dequeue();
     document.head.appendChild(sdkScript);
 })();
